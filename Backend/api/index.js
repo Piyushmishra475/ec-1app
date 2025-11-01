885444d74205c9e409fd07a2ec340c091d239e15
Piyush <Piyushmishra475@hmail.com>
2025-11-01 20:17:31 +0530

    Fix backend deployment

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -1,3 +1,31 @@
-import app from '../Server.js';
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from '../config/mongodb.js'; 
+import connectCloudinary from '../config/cloudinary.js';
+import userRouter from '../routes/userRoute.js';
+import productRouter from '../routes/productRoute.js';
+import cartRouter from '../routes/cartRoute.js';
+import orderRouter from '../routes/orderRoute.js';
+
+const app = express()
+
+// Initialize connections
+connectDB()
+connectCloudinary()
+
+// middlewares 
+app.use(express.json())
+app.use(cors())
+
+// api end points
+app.use('/api/user',userRouter)
+app.use('/api/product',productRouter)
+app.use('/api/cart',cartRouter)
+app.use('/api/order',orderRouter)
+
+app.get('/',(req,res)=>{
+    res.send('API Working')
+})
 
 export default app;
